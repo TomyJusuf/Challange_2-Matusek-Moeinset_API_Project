7e9b3e5f1b5a9200027b90db8b4b30c408cacc4c
TomasMatusek <Tomas.Matusek@edu.fh-kaernten.ac.at>
2023-12-17 15:10:54 +0100

    Create Error handlig statement to appTesting..

--- backend_side/appTestingImage.js
+++ backend_side/appTestingImage.js
@@ -5,24 +5,27 @@ const API_URL = 'http://localhost:3000/api/get-data';
 async function showData() {
   try {
     const response = await fetch(API_URL);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
     const data = await response.json();
 
     let html = ''; // Initialize an empty string to concatenate HTML content
 
     for (let i = 0; i < data.length; i++) {
       console.log(data[i]);
-      console.log(`https://sumsi.dev.webundsoehne.com${data[i]}`);
+
       html += `
         <div class="imageContainer">
           <img class="imageBox-1" src="https://sumsi.dev.webundsoehne.com${data[i].image.public_location}"></img>
-         
         </div>`;
     }
 
     demo.innerHTML = html; // Set the HTML content after the loop
   } catch (error) {
     console.error('Error fetching data:', error);
+    if (error instanceof TypeError) {
+      console.error('Check if the API server is running and accessible.');
+    }
   }
 }
-
-showData();
