597e116538e05ff6bfe371815c32758156cabcbc
TomasMatusek <Tomas.Matusek@edu.fh-kaernten.ac.at>
2023-12-17 10:33:27 +0100

    Create route file with middleware and export that to server.js

---
+++ backend_side/route.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { getData } from './service.js';
+const router = express.Router();
+
+const GET_ALL_DATA = '/get-data';
+
+// Middleware function for logging requests
+router.use((req, res, next) => {
+  console.log('Request received:', req.method, req.url);
+  //   console.log('Request body:', res.body);
+  next();
+});
+
+router.get(GET_ALL_DATA, getData);
+
+export default router;
