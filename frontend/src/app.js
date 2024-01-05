ad3fa1cdc6a052db877088613f48711594e8ed69
TomasMatusek <Tomas.Matusek@edu.fh-kaernten.ac.at>
2024-01-05 18:14:21 +0100

    testing app.js TomasMatusek

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,18 +1,18 @@
-window.addEventListener("DOMContentLoaded", async () => {
+window.addEventListener('DOMContentLoaded', async () => {
   await showData();
   await determineWinnerAndRest();
 });
-const demo = document.querySelector(".demo");
-const API_URL = "https://sumsi.dev.webundsoehne.com/api/v1/submissions";
+const demo = document.querySelector('.demo');
+const API_URL = 'https://sumsi.dev.webundsoehne.com/api/v1/submissions';
 let submissionId;
 
 const DB_token =
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiZDhjYmMzNTVmYzcxYTk1YjU5MWNkZDBmNzBjNTI4ZjVkNDU5NDU2MTBlNWMwNTRjOTZhOTZiZmQ2NzY4NTE5MjU5ZmI3YWY1NzIxMTAxMGYiLCJpYXQiOjE3MDI3NTc3NzEuNzM5OTQsIm5iZiI6MTcwMjc1Nzc3MS43Mzk5NDMsImV4cCI6MTczNDM4MDE3MS43MzM4NjUsInN1YiI6IjEiLCJzY29wZXMiOltdfQ.qldlYkU025o_qa_0mqUFr-J_Bam6sPMjrNoz-WPcVO8Z81Ur0zuAt4rAr_qZnL1lojE1eyuCCw-YwCkL6arpryV0z1yJ3pXpSVwb8zppusTbjjvWFCNfZWPnB7s8N5KzoUGRcnfq4_T0he_oP4SPrQfjN8QLMtJwfA6eByXyhmB20jhmbXgNcVOWiKDGO1E14TQA4jKER3DhEUD4j9huq3ruGCWJlzBRBpFqSY4GP8-6GTKshIsyUg4vyzrnrgIGLR7nhxzp5XFL647OdBRKuvRb72Kmnj98vglPqNmvBs-M0_xzTl5yusezktX00A6cOO8czgsnipS_q-fqMDyOpdea7bIa7bHbIFTKDavV7YsS1EHRQ1djKnpgqFAi8642uoeh7wvTtvpOjWQhp_3_q3Gt4Cm3pOUrUd8CPAYcbQiDQEyhV7apOU5pr0DBSDqRgaw7ggeUSpQU6O0dY3I25GzWWevRekF1IcBUHprRrErL81GhZbrGUHlPk2ULo7z8JeNG4SwSTa-RRuaaV8AKU5SpgHe3TnQLOimF8l5r9wOUTPcoqnTI_s-Xox9CdBD4WXh3k4Pk67_L4o7TDa6TVcfrk728yIQl0XGr_xz_LRDKQuCGXVbfUTO7oZWbm95aOqL5zcbQVVM5roHyUUA7u5AGqc-u1x3wtixpnI950zc";
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiZDhjYmMzNTVmYzcxYTk1YjU5MWNkZDBmNzBjNTI4ZjVkNDU5NDU2MTBlNWMwNTRjOTZhOTZiZmQ2NzY4NTE5MjU5ZmI3YWY1NzIxMTAxMGYiLCJpYXQiOjE3MDI3NTc3NzEuNzM5OTQsIm5iZiI6MTcwMjc1Nzc3MS43Mzk5NDMsImV4cCI6MTczNDM4MDE3MS43MzM4NjUsInN1YiI6IjEiLCJzY29wZXMiOltdfQ.qldlYkU025o_qa_0mqUFr-J_Bam6sPMjrNoz-WPcVO8Z81Ur0zuAt4rAr_qZnL1lojE1eyuCCw-YwCkL6arpryV0z1yJ3pXpSVwb8zppusTbjjvWFCNfZWPnB7s8N5KzoUGRcnfq4_T0he_oP4SPrQfjN8QLMtJwfA6eByXyhmB20jhmbXgNcVOWiKDGO1E14TQA4jKER3DhEUD4j9huq3ruGCWJlzBRBpFqSY4GP8-6GTKshIsyUg4vyzrnrgIGLR7nhxzp5XFL647OdBRKuvRb72Kmnj98vglPqNmvBs-M0_xzTl5yusezktX00A6cOO8czgsnipS_q-fqMDyOpdea7bIa7bHbIFTKDavV7YsS1EHRQ1djKnpgqFAi8642uoeh7wvTtvpOjWQhp_3_q3Gt4Cm3pOUrUd8CPAYcbQiDQEyhV7apOU5pr0DBSDqRgaw7ggeUSpQU6O0dY3I25GzWWevRekF1IcBUHprRrErL81GhZbrGUHlPk2ULo7z8JeNG4SwSTa-RRuaaV8AKU5SpgHe3TnQLOimF8l5r9wOUTPcoqnTI_s-Xox9CdBD4WXh3k4Pk67_L4o7TDa6TVcfrk728yIQl0XGr_xz_LRDKQuCGXVbfUTO7oZWbm95aOqL5zcbQVVM5roHyUUA7u5AGqc-u1x3wtixpnI950zc';
 
 let headers = {
-  "Cache-Control": "max-age=31536000, must-revalidate",
-  "Content-Type": "application/json",
-  Accept: "application/json",
+  'Cache-Control': 'max-age=31536000, must-revalidate',
+  'Content-Type': 'application/json',
+  Accept: 'application/json',
   Authorization: `Bearer ${DB_token}`, // Include the token in the Authorization header
 };
 
@@ -30,7 +30,7 @@ async function showData() {
     console.log(saveData);
     // console.log(saveData);
     const index = saveData.findIndex(
-      (todo) => todo.email === localStorage.getItem("savedEmail")
+      (todo) => todo.email === localStorage.getItem('savedEmail')
     );
 
     if (index == -1) {
@@ -39,7 +39,7 @@ async function showData() {
       submissionId = saveData[index].id;
     }
 
-    let html = "";
+    let html = '';
     for (let i = 0; i < saveData.length; i++) {
       // console.log(i);
       html += `
@@ -60,18 +60,19 @@ async function showData() {
     }
     demo.innerHTML = html;
 
-    const imageContainers = document.querySelectorAll(".even-picture");
+    const imageContainers = document.querySelectorAll('.even-picture');
 
     imageContainers.forEach((element, i) => {
-      element.addEventListener("click", () => {
+      element.addEventListener('click', () => {
         email = saveData[i].email; // testing
         elementID = element.id;
+        console.log(email);
       });
     });
   } catch (error) {
-    console.error("Error fetching data:", error);
+    console.error('Error fetching data:', error);
     if (error instanceof TypeError) {
-      console.error("Check if the API server is running and accessible.");
+      console.error('Check if the API server is running and accessible.');
     }
   }
 }
@@ -86,7 +87,7 @@ async function voting(submissionId) {
   };
   try {
     const response = await fetch(`${API_URL}/${submissionId}/votings`, {
-      method: "POST",
+      method: 'POST',
       headers,
       body: JSON.stringify(body),
     });
@@ -94,25 +95,25 @@ async function voting(submissionId) {
       console.error(`HTTP error! Status: ${response.status}`);
       // Log the entire response for debugging purposes
 
-      if (response.headers.get("Content-Type")?.includes("application/json")) {
+      if (response.headers.get('Content-Type')?.includes('application/json')) {
         try {
           const responseText = await response.text();
           const validationErrors = JSON.parse(responseText);
-          console.error("Validation Errors:", validationErrors);
+          console.error('Validation Errors:', validationErrors);
           // Display validation errors to the user or handle them accordingly
         } catch (jsonError) {
-          console.error("Error parsing JSON:", jsonError);
+          console.error('Error parsing JSON:', jsonError);
         }
       } else {
-        console.error("Response is not in JSON format.");
+        console.error('Response is not in JSON format.');
         // Handle non-JSON response as needed
       }
     } else {
       const responseData = await response.json();
-      console.log("Response data:", responseData);
+      console.log('Response data:', responseData);
     }
   } catch (error) {
-    console.error("Error:", error);
+    console.error('Error:', error);
   }
 }
 
@@ -120,7 +121,7 @@ async function voting(submissionId) {
 async function getVotes() {
   try {
     const response = await fetch(`${API_URL}/${submissionId}/votings`, {
-      method: "GET",
+      method: 'GET',
       headers,
     });
     // ERROR HANDLING
@@ -128,10 +129,10 @@ async function getVotes() {
       console.error(`HTTP error! Status: ${response.status}`);
     } else {
       const responseData = await response.json();
-      console.log("Votes:", responseData);
+      console.log('Votes:', responseData);
     }
   } catch (error) {
-    console.error("Error:", error);
+    console.error('Error:', error);
   }
 }
 
@@ -142,7 +143,7 @@ async function countVotes() {
       `https://sumsi.dev.webundsoehne.com/api/v1/submissions/${submissionId}/votes/count`
     );
     const response = await fetch(url, {
-      method: "GET",
+      method: 'GET',
       headers,
     });
 
@@ -152,45 +153,45 @@ async function countVotes() {
       // Log the entire response for debugging purposes
     } else {
       const responseData = await response.json();
-      console.log("Vote count:", responseData.data.votes);
+      console.log('Vote count:', responseData.data.votes);
     }
   } catch (error) {
-    console.error("Error:", error);
+    console.error('Error:', error);
   }
 }
 
 //submit
 async function submitForm() {
   // Get the form element by its ID
-  const firstName = document.getElementById("legalguardian_firstname").value;
-  const lastName = document.getElementById("legalguardian_lastname").value;
-  const email = document.getElementById("email").value;
-  const child_firstname = document.getElementById("child_firstname").value;
-  const child_age = document.getElementById("child_age").value;
+  const firstName = document.getElementById('legalguardian_firstname').value;
+  const lastName = document.getElementById('legalguardian_lastname').value;
+  const email = document.getElementById('email').value;
+  const child_firstname = document.getElementById('child_firstname').value;
+  const child_age = document.getElementById('child_age').value;
 
   // Create FormData using the form element
   const postData = new FormData();
-  postData.append("legalguardian_firstname", firstName);
-  postData.append("legalguardian_lastname", lastName);
-  postData.append("email", email);
-  postData.append("child_firstname", child_firstname);
-  postData.append("child_age", child_age);
-  postData.append("approval_privacypolicy", "1");
-  postData.append("approval_participation", "1");
-  postData.append("approval_mailnotification", "1");
+  postData.append('legalguardian_firstname', firstName);
+  postData.append('legalguardian_lastname', lastName);
+  postData.append('email', email);
+  postData.append('child_firstname', child_firstname);
+  postData.append('child_age', child_age);
+  postData.append('approval_privacypolicy', '1');
+  postData.append('approval_participation', '1');
+  postData.append('approval_mailnotification', '1');
   postData.append(
-    "image",
+    'image',
     document.querySelector('input[name="image"]').files[0]
   );
-  localStorage.setItem("savedEmail", email);
+  localStorage.setItem('savedEmail', email);
   console.log(postData);
   try {
     const response = await fetch(
-      "https://sumsi.dev.webundsoehne.com/api/v1/submissions",
+      'https://sumsi.dev.webundsoehne.com/api/v1/submissions',
       {
-        method: "POST",
+        method: 'POST',
         headers: {
-          Accept: "application/json",
+          Accept: 'application/json',
           Authorization: `Bearer ${DB_token}`,
         },
         body: postData,
@@ -201,31 +202,31 @@ async function submitForm() {
       console.error(`HTTP error! Status: ${response.status}`);
       // Log the entire response for debugging purposes
 
-      if (response.headers.get("Content-Type")?.includes("application/json")) {
+      if (response.headers.get('Content-Type')?.includes('application/json')) {
         try {
           const responseText = await response.text();
           const validationErrors = JSON.parse(responseText);
-          console.error("Validation Errors:", validationErrors);
+          console.error('Validation Errors:', validationErrors);
         } catch (jsonError) {
-          console.error("Error parsing JSON:", jsonError);
+          console.error('Error parsing JSON:', jsonError);
         }
       } else {
-        console.error("Response is not in JSON format.");
+        console.error('Response is not in JSON format.');
         // Handle non-JSON response as needed
       }
     } else {
       const responseData = await response.json();
-      console.log("Response data:", responseData);
+      console.log('Response data:', responseData);
     }
   } catch (error) {
-    console.error("Error:", error);
+    console.error('Error:', error);
   }
   showData();
 }
 
 // DELETE
 async function deleteFn() {
-  const localStorageData = localStorage.getItem("savedEmail");
+  const localStorageData = localStorage.getItem('savedEmail');
   const index = saveData.findIndex((todo) => todo.email === localStorageData);
   console.log(index);
   console.log(submissionId);
@@ -237,17 +238,17 @@ async function deleteFn() {
       // Assuming 'submissionId' is defined somewhere in your code
       const response = await fetch(`${API_URL}/${submissionId}`, {
         headers,
-        method: "DELETE",
+        method: 'DELETE',
       });
 
       const data = await response.json();
-      localStorage.removeItem("savedEmail");
+      localStorage.removeItem('savedEmail');
       console.log(data);
     } catch (error) {
-      console.error("Error:", error);
+      console.error('Error:', error);
     }
   } else {
-    console.log("Item not found in saveData");
+    console.log('Item not found in saveData');
   }
   showData();
 }
@@ -257,7 +258,7 @@ async function deleteAllItems() {
     for (const item of saveData) {
       const response = await fetch(`${API_URL}/${item.id}`, {
         headers,
-        method: "DELETE",
+        method: 'DELETE',
       });
 
       const data = await response.json();
@@ -268,11 +269,11 @@ async function deleteAllItems() {
     saveData = [];
 
     // Clear localStorage if needed
-    localStorage.removeItem("savedEmail");
+    localStorage.removeItem('savedEmail');
 
-    console.log("All items deleted successfully");
+    console.log('All items deleted successfully');
   } catch (error) {
-    console.error("Error:", error);
+    console.error('Error:', error);
   }
 }
 //Winner
@@ -309,7 +310,7 @@ async function determineWinnerAndRest() {
   );
 
   let winEmail;
-  let html = "";
+  let html = '';
   for (let i = 0; i < sortedEmails.length; i++) {
     winEmail = sortedEmails[i];
     const index = saveData.findIndex((item) => item.email === winEmail);
@@ -329,8 +330,8 @@ async function determineWinnerAndRest() {
     }
   }
 
-  document.getElementById("winnerOfGallery").innerHTML = html;
+  document.getElementById('winnerOfGallery').innerHTML = html;
 }
 // document.getElementById('showData').addEventListener('click', showData);
 // document.getElementById('vote').addEventListener('click', voting);
-document.getElementById("submit").addEventListener("click", submitForm);
+document.getElementById('submit').addEventListener('click', submitForm);
